6cd64102024a4ee8f9e84e2bed333d51666985cf
Kris Olszewski <kris.olszewski@publicishawkeye.com>
2016-12-12 11:29:26 -0600

    Add env options. Add local VM server. Add comments.

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,10 @@
  * -----------------------------------------------------------------------------
  */
 
+const bs = require('browser-sync');
 const changed = require('gulp-changed');
 const del = require('del');
+const minimist = require('minimist');
 const pages = require('gulp-gh-pages');
 const gulp = require('gulp');
 const pug = require('gulp-pug');
@@ -21,12 +23,25 @@ const path = {
 };
 
 /**
+ * Build options and environments
+ * -----------------------------------------------------------------------------
+ */
+
+const options = minimist(process.argv.slice(2), {
+  string: [ 'env' ],
+  default: {
+    env: 'dev',
+  },
+});
+
+/**
  * Default task
  * -----------------------------------------------------------------------------
  */
 
 gulp.task('default', (callback) => sequence(
   [ 'build' ],
+  [ 'server' ],
   callback
 ));
 
@@ -54,6 +69,27 @@ gulp.task('build', (callback) => sequence(
 ));
 
 /**
+ * Server
+ * -----------------------------------------------------------------------------
+ */
+
+gulp.task('server', () => {
+  // Create and initialize local VM server
+  bs.create();
+  bs.init({
+    notify: false,
+    server: `./${path.build}`,
+    open: 'local',
+    ui: false,
+  });
+  // Watch for file changes and run corresponding tasks
+  gulp.watch(`./${path.src}/misc/**/*`, [ 'misc' ]);
+  gulp.watch(`./${path.src}/views/**/*`, [ 'views' ]);
+  // Watch build changes and reload browser
+  bs.watch(`${path.build}/**/*`).on('change', bs.reload);
+});
+
+/**
  * Wipe build
  * -----------------------------------------------------------------------------
  */
@@ -66,8 +102,11 @@ gulp.task('clean', () => del(`./${path.build}`));
  */
 
 gulp.task('misc', () => gulp
+// Select source files
   .src(`${path.src}/misc/**/*`, { dot: true })
+  // Check which files have changed
   .pipe(changed(path.build))
+  // Save build files
   .pipe(gulp.dest(path.build))
 );
 
@@ -77,11 +116,20 @@ gulp.task('misc', () => gulp
  */
 
 gulp.task('views', () => gulp
+  // Select source files
   .src(`${path.src}/views/site/**/*.pug`)
+  // Check which files have changed
   .pipe(changed(path.build, {
     extension: '.html',
   }))
-  .pipe(pug())
+  // Compile Pug to HTML
+  .pipe(pug({
+    pretty: (options.env === 'dev') ? true : false,
+    data: {
+      env: options.env,
+    },
+  }))
+  // Save build files
   .pipe(gulp.dest(path.build))
 );
 
@@ -91,7 +139,9 @@ gulp.task('views', () => gulp
  */
 
 gulp.task('gh-pages', () => gulp
+  // Select build files
   .src(`${path.build}/**/*`)
+  // Push `build` files to `master` branch
   .pipe(pages({
     branch: 'master',
   }))
